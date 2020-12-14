c2ca9b7eca4894e7def86b41dd230ac28445e6f1
salty-Frankenstein <1328955419@qq.com>
2020-12-14 19:58:47 +0800

    update mifgen

--- mifgen/mifgen.hs
+++ mifgen/mifgen.hs
@@ -1,14 +1,6 @@
 import Data.Char
 import Text.Printf
 
-{-
-WIDTH=12;
-DEPTH=4096;
-ADDRESS_RADIX=HEX;
-DATA_RADIX=HEX;
-CONTENT
--}
-
 readHexCode :: FilePath -> IO [String]
 readHexCode f = do
   c <- readFile f
@@ -17,16 +9,20 @@ readHexCode f = do
 writeMif :: FilePath -> [String] -> IO ()
 writeMif f c = do
   let width = "WIDTH=32;\n"
-  let depth = "DEPTH=" ++ (show $ length c) ++ ";\n"
+  let depth = "DEPTH=" ++ show (length c) ++ ";\n"
   let ar = "ADDRESS_RADIX=HEX;\n"
   let dr = "DATA_RADIX=HEX;\n"
-  let content = ""
-  let mifData = concat $ zipWith (\a b -> a ++ " : " ++ b ++ "\n") (map (printf "%03X" :: Int -> String) [0..]) c
-  -- writeFile f (width ++ depth ++ ar ++ dr ++)
+  let mifData = concat $ zipWith toLine (map toIndex [0 ..]) c
+  let content = "CONTENT\nBEGIN\n" ++ mifData ++ "END;\n"
+  writeFile f (width ++ depth ++ ar ++ dr ++ content)
   return ()
+  where
+    toLine a b = a ++ " : " ++ b ++ "\n"
+
+    toIndex :: Int -> String
+    toIndex = printf "%03X"
 
 main :: IO ()
-main = do
-  s <- readHexCode "./code/fib_h.txt"
-  putStrLn $ concat $ zipWith (\a b -> a ++ " : " ++ b ++ "\n") (map (printf "%03X" :: Int -> String) [0..]) s
-  return ()
+main =
+  readHexCode "./code/fib_h.txt"
+    >>= writeMif "./code/fib.mif"
